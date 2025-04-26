f51be620731172fd38028a0fcf375a71edc73d06
cssssia <cassia.toscani@gmail.com>
2025-04-26 17:59:09 -0300

    cronometer

---
+++ Assets/Project/Scripts/UI/CronometerController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CronometerController : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> m_numbersSprites;
+    [SerializeField] private Image m_tenSpriteRenderer;
+    [SerializeField] private Image m_unitSpriteRenderer;
+    [SerializeField] private Animator animator;
+
+    public void StartCronometer(int p_seconds)
+    {
+        StartCoroutine(RunCronometer(p_seconds));
+    }
+
+    public IEnumerator RunCronometer(int p_seconds)
+    {
+        for (int i = p_seconds; i >= 0; i--)
+        {
+            if (i >= 10)
+            {
+                int l_ten = int.Parse(i.ToString().Substring(0, 1));
+                int l_unit = int.Parse(i.ToString().Substring(1, 1));
+
+                Debug.Log(l_ten + " " + l_unit);
+
+                m_tenSpriteRenderer.sprite = m_numbersSprites[l_ten];
+                m_unitSpriteRenderer.sprite = m_numbersSprites[l_unit];
+            }
+            else
+            {
+                m_tenSpriteRenderer.sprite = m_numbersSprites[0];
+                m_unitSpriteRenderer.sprite = m_numbersSprites[i];
+            }
+
+            animator.enabled = true;
+
+            yield return new WaitForSeconds(1f);
+        }
+
+        animator.enabled = false;
+
+    }
+}
