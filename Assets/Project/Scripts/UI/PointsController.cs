481a3d3499c1b915b98aa645fbd6bfa395a54ae2
cssssia <cassia.toscani@gmail.com>
2025-04-26 15:08:33 -0300

    pivots and hearts

--- Assets/Project/Scripts/UI/PointsController.cs
+++ Assets/Project/Scripts/UI/PointsController.cs
@@ -1,25 +1,55 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointsController : Singleton<PointsController>
 {
     [SerializeField] private int m_life = 10;
+    [SerializeField] private int m_numOfHearts;
     [SerializeField] private int m_penality;
 
     [SerializeField] private TextMeshProUGUI m_lifeText;
     [SerializeField] private TextMeshProUGUI m_penalityText;
+    [SerializeField] private List<Image> m_heartContainers;
 
     void Start()
     {
         EnemyManager.Instance.OnEnemyReachLaneEnd += OnEnemyReachLaneEnd;
+
+        for (int i = 0; i < m_heartContainers.Count; i++)
+        {
+            if (i < m_numOfHearts)
+            {
+                m_heartContainers[i].enabled = true;
+            }
+            else
+            {
+                m_heartContainers[i].enabled = false;
+            }
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < m_heartContainers.Count; i++)
+        {
+            if (i < m_life)
+            {
+                m_heartContainers[i].enabled = true;
+            }
+            else
+            {
+                m_heartContainers[i].enabled = false;
+            }
+        }
     }
 
     private void OnEnemyReachLaneEnd(int p_point)
     {
         m_life -= p_point;
-        m_lifeText.text = m_life.ToString();
     }
 
     public int Penality()
